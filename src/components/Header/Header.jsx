7ff1b878dfab19bb23c11d85377bafb921d61e35
David Fernadez <daffevafernandez@gmail.com>
2024-09-24 16:02:27 +0200

    feat: creation of the view and register route

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,12 @@ import { useNavigate } from 'react-router-dom'
 export const Header = () => {
 
     const navigate = useNavigate();
-    
+
     return (
         <>
             <ul>
-                <div onClick={()=>navigate('/')}>Login</div>
+                <div onClick={()=>navigate('/login')}>Login</div>
+                <div onClick={()=>navigate('/register')}>Register</div>
             </ul>
         </>
     )
