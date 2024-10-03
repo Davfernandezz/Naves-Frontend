29368a0c7849653a2cfc850c3a1d4cddc056a290
David Fernadez <daffevafernandez@gmail.com>
2024-10-03 17:01:08 +0200

    feat: myHistory design

--- src/views/MyHistory/MyHistory.jsx
+++ src/views/MyHistory/MyHistory.jsx
@@ -5,51 +5,58 @@ import { getAccessHistory } from '../../services/personApiCalls';
 import "./MyHistory.css";
 
 export const MyHistory = () => {
-    const [accessHistory, setAccessHistory] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-  
-    const { passport } = useAuth();
-    const navigate = useNavigate();
-  
-    useEffect(() => {
-      if (!passport || !passport.token) {
-        navigate("/login");
+  const [accessHistory, setAccessHistory] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [visibleItems, setVisibleItems] = useState(2);
+
+  const { passport } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!passport || !passport.token) {
+      navigate("/login");
+    } else {
+      fetchAccessHistory();
+    }
+  }, [passport, navigate]);
+
+  const fetchAccessHistory = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const userId = passport.tokenData.id;
+      const response = await getAccessHistory(userId, passport.token);
+
+      if (response.success) {
+        setAccessHistory(response.data.access_history);
       } else {
-        fetchAccessHistory();
+        setError(response.message || "Failed to fetch access history");
       }
-    }, [passport, navigate]);
-  
-    const fetchAccessHistory = async () => {
-      setIsLoading(true);
-      setError(null);
-      try {
-        const userId = passport.tokenData.id;
-        const response = await getAccessHistory(userId, passport.token);
-  
-        if (response.success) {
-          setAccessHistory(response.data.access_history);
-        } else {
-          setError(response.message || "Failed to fetch access history");
-        }
-      } catch (error) {
-        console.error("Error fetching access history:", error);
-        setError("An error occurred while fetching access history");
-      }
-      setIsLoading(false);
-    };
-  
-    return (
-      <div className="my-history-wrapper">
-        <div className="my-history-container">
-          <h1>My Access History</h1>
-          {isLoading && <p>Loading...</p>}
-          {error && <p className="error">Error: {error}</p>}
-          {accessHistory && accessHistory.length > 0 ? (
+    } catch (error) {
+      console.error("Error fetching access history:", error);
+      setError("An error occurred while fetching access history");
+    }
+    setIsLoading(false);
+  };
+
+  const loadMore = () => {
+    setVisibleItems(prevVisibleItems => prevVisibleItems + 2);
+  };
+
+  return (
+    <div className="history-wrapper">
+      <div className="history-container">
+        <h1 className="history-title text-center mb-2">My Access History</h1>
+        <h2 className="history-subtitle text-center mb-4">Recent Entries and Exits</h2>
+        {isLoading && <p className="text-center">Loading...</p>}
+        {error && <p className="error text-center">Error: {error}</p>}
+        {accessHistory && accessHistory.length > 0 ? (
+          <>
             <ul className="history-list">
-              {accessHistory.map((access, index) => (
-                <li key={index} className="history-item">
-                  <h3>Room: {access.room_name}</h3>
+              {accessHistory.slice(0, visibleItems).map((access, index) => (
+                <li key={index} className="history-item mb-4">
+                  <h3 className="history-item-title">{access.room_name}</h3>
                   <p>Room ID: {access.room_id}</p>
                   <p>Entry: {new Date(access.entry_datetime).toLocaleString()}</p>
                   {access.exit_datetime && (
@@ -58,10 +65,18 @@ export const MyHistory = () => {
                 </li>
               ))}
             </ul>
-          ) : (
-            <p>No access history available.</p>
-          )}
-        </div>
+            {visibleItems < accessHistory.length && (
+              <div className="text-center mt-4">
+                <button onClick={loadMore} className="btn btn-primary load-more-button">
+                  Load More
+                </button>
+              </div>
+            )}
+          </>
+        ) : (
+          <p className="text-center">No access history available.</p>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
+}
